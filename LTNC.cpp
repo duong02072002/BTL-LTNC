7dd8aa9d33ae612a864b12397399d7c1b6f57320
duong02072002 <83348698+duong02072002@users.noreply.github.com>
2021-05-12 09:29:45 +0700

    thaydoi

--- LTNC.cpp
+++ LTNC.cpp
@@ -13,6 +13,9 @@ typedef struct Giohang_st{
 }GH;
 void nhapKhachHang(GH &input);
 void nhapSP(SP &input);
+void inThongTinGioHang(GH output);
+void tinhTien(SP &input);
+void tongTienCanThanhToan(GH source);
 int main(){
     GH danhsach;
     int input;
@@ -46,4 +49,25 @@ void nhapSP(SP &input){
     scanf("%d",&input.sl);
     printf("\nnhap gia: ");
     scanf("%d",&input.gia);
+    tinhTien(input);
+}
+void inThongTinGioHang(GH output){
+	printf("\n--------------------Thong Tin Gio Hang-------------------");
+	printf("\nTen khach hang: %s", output.tenKhach);
+	printf("\nDia chi: %s", output.diaChi);
+	printf("\nSTT||ID\t\t||Ten\t\t||Gia\t\t||SL\t\t||Tong");
+	for(int i = 0; i<output.soluong_sp;i++){
+		printf("\n00%d||%s\t\t||%s\t\t||%d\t\t||%d\t\t||%d",i+1,output.ds_hang[i].msp,output.ds_hang[i].tensp,output.ds_hang[i].gia,output.ds_hang[i].sl,output.ds_hang[i].tongtien);
+	}
+	printf("\n---------------------------------------------------------\n");
+}
+void tinhTien(SP &input){
+	input.tongtien = input.gia * input.sl;
+}
+void tongTienCanThanhToan(GH source){
+	source.tongTien_tt = 0;
+	for(int i = 0; i<source.soluong_sp;i++){
+		source.tongTien_tt += source.ds_hang[i].tongtien;
+	}
+	printf("\nTong tien khach can thanh toan la: %d", source.tongTien_tt);
 }
